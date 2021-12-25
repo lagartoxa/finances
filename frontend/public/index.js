1e84bf609a6ca649df6094f0f81c4ddccaa92226
Ildomar Carvalho <ildomar.gomes@pollux.com.br>
2021-12-25 15:10:13 -0300

    Adding AdminLTE to the project

---
+++ frontend/public/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Users } from "../src";
+
+
+const App = () => {
+    return <Users />
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
